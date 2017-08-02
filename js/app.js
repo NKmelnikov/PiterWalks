2654fb08056da14fe15b885739c33b49ca1e44ef
Nikita <Nkmelnikov@hotmail.com>
2017-08-02 21:14:02 +0300

    Getting second section ready

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
+//-------отрисовка диагоналей---------------
+
 var angle = Math.atan2($(window).width(), $(window).height());
+console.log(angle);
 $('.diagonal-box-1,.diagonal-box-2').css('transform', 'skew(' + angle + 'rad)').show();
+
+//---------------------------------
+
+$('#second-section').bind('inview', function (event, visible) {
+	console.log(1);
+	if (visible == true) {
+		console.log(2);
+
+		$('#page-counter').addClass('orange');
+	} else {
+		// element has gone out of viewport
+	}
+});
