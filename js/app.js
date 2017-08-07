b1fd4fffce00349fe7db60d7fefad422126cf348
Nikita <Nkmelnikov@hotmail.com>
2017-08-08 00:07:39 +0300

    scew_solution(not solved)

--- js/app.js
+++ js/app.js
@@ -6,10 +6,16 @@ $('.diagonal-box-1,.diagonal-box-2,.diagonal-box-3,.diagonal-box-4').css('transf
 
 //---------------------------------
 
-$('.up-part,.mid-part,.bot-part').each(function () {
-	$(this).on("mouseenter", function() {
-		$(this).css('opacity','1');
-	}).on('mouseleave', function() {
-		$(this).css('opacity','.1');
-	});
+// $('.up-part,.mid-part,.bot-part').each(function () {
+// 	$(this).on("mouseenter", function() {
+// 		$(this).css('opacity','1');
+// 	}).on('mouseleave', function() {
+// 		$(this).css('opacity','.1');
+// 	});
+// });
+$('.two-mid').click(function () {
+	console.log(2);
+});
+$('.three-mid').click(function () {
+	console.log(3);
 });
