d4136ae25b5cde80bf4ba97bda7b52285ec43e1c
Nikita <Nkmelnikov@hotmail.com>
2017-08-10 08:12:01 +0300

    hover images

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ $('.diagonal-box-1,.diagonal-box-2,.diagonal-box-3,.diagonal-box-4').css('transf
 
 $('.up-part div,.mid-part div,.bot-part div').each(function () {
 	$(this).on("mouseenter", function() {
-		$(this).css({'opacity':'1','overflow':'visible'});
+		$(this).css({'opacity':'1'});
 	}).on('mouseleave', function() {
-		$(this).css({'opacity': '.1','overflow':'hidden'});
+		$(this).css({'opacity': '.1'});
 	});
 });
 $('.triangle').click(function () {
