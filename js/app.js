24d0ebe76013f1b6e76a6c6f43b0f7b6a5ee2687
Nikita <Nkmelnikov@hotmail.com>
2017-08-16 10:55:54 +0300

    svg 0.1

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 //-------отрисовка диагоналей---------------
 
 var angle = Math.atan2($(window).width(), $(window).height());
-console.log(angle);
 $('.diagonal-box-1,.diagonal-box-2,.diagonal-box-3,.diagonal-box-4').css('transform', 'skew(' + angle + 'rad)').show();
 
 //---------------------------------
 
 $('.up-part div,.mid-part div,.bot-part div').each(function () {
 	$(this).on("mouseenter", function() {
-		$(this).css({'opacity':'1','overflow':'visible'});
+		$(this).css({'opacity':'1'});
 	}).on('mouseleave', function() {
-		$(this).css({'opacity': '.1','overflow':'hidden'});
+		$(this).css({'opacity': '.1'});
 	});
 });
 $('.triangle').click(function () {
